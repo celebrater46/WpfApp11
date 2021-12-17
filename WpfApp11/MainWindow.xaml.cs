b5e4dfa1c25e8c32e02450574941b454524cd05a
hideru <jota.16.stigma.world@gmail.com>
2021-12-17 18:25:05 +0900

    wrote code

--- WpfApp11/MainWindow.xaml.cs
+++ WpfApp11/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace WpfApp11
         {
             InitializeComponent();
         }
+
+        private void LoadFromConfig()
+        {
+            var cfg = Config.ReadConfig();
+            if (cfg == null)
+            {
+                // No Setting
+                return;
+            }
+
+            nameText.Text = cfg.Person.Name;
+            ageText.Text = cfg.Person.Age.ToString();
+            cal.SelectedDate = cfg.Person.Birthday.ToLocalTime();
+            memoText.Text = cfg.Memo;
+        }
+
+        private void SaveToConfig()
+        {
+            var cfg = new Config();
+
+            cfg.Person.Name = nameText.Text;
+            int age;
+            if (int.TryParse(ageText.Text, out age) == false)
+            {
+                // when convert failed
+                age = 0;
+            }
+
+            cfg.Person.Age = age;
+            cfg.Person.Birthday = (DateTime)cal.SelectedDate;
+            cfg.Memo = memoText.Text;
+            
+            // Save Config
+            Config.WriteConfig(cfg);
+        }
+
+        private void WindowContentRendered(object sender, EventArgs e)
+        {
+            LoadFromConfig();
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            SaveToConfig();
+        }
     }
 }
