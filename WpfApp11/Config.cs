ce9b67ce4b8f4083420891df761e2b5796636c51
hideru <jota.16.stigma.world@gmail.com>
2021-12-21 18:50:38 +0900

    commit before changing to system.text.json

--- WpfApp11/Config.cs
+++ WpfApp11/Config.cs
@@ -21,54 +21,54 @@ public class Config
         this.Person = new Person();
     }
 
-    // public static string GetConfigFilePath()
-    public static void GetConfigFilePath()
+    public static string GetConfigFilePath()
+    // public static void GetConfigFilePath()
     {
         // .exe location
         // string appFilePath = System.Reflection.Assembly.GetEntryAssembly().Location; // Dereference of a possibly null reference
         // return System.Text.RegularExpressions.Regex.Replace(appFilePath, ".exe", "json",
         //     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
-        // try
-        // {
-        //     string appFilePath = System.Reflection.Assembly.GetEntryAssembly().Location; // Dereference of a possibly null reference
-        //     return System.Text.RegularExpressions.Regex.Replace(appFilePath, ".exe", "json",
-        //         System.Text.RegularExpressions.RegexOptions.IgnoreCase);
-        // }
-        // catch (Exception e)
-        // {
-        //     return e.Message;
-        // }
+        try
+        {
+            string appFilePath = System.Reflection.Assembly.GetEntryAssembly().Location; // Dereference of a possibly null reference
+            return System.Text.RegularExpressions.Regex.Replace(appFilePath, ".exe", "json",
+                System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
     }
 
     // public static Config ReadConfig() // Possible null reference return
-    // public static Config? ReadConfig()
-    // {
-    //     string configFile = GetConfigFilePath();
-    //     if (File.Exists(configFile) == false)
-    //     {
-    //         return null;
-    //     }
-    //
-    //     using (var reader = new StreamReader(configFile, Encoding.UTF8))
-    //     {
-    //         // Read settings
-    //         string buf = reader.ReadToEnd();
-    //         
-    //         // Deserialize and return
-    //         // var js = new System.Web.Script.Serialization.JavaScriptSerializer(); // Not found System.Web.Extensions (Visual Studio as well)
-    //         // var js = JsonConvert.DeserializeObject(json);
-    //         return JsonConvert.DeserializeObject<Config>(buf); // Use "Newtonsoft.Json" instead of System.Web.Extensions
-    //     }
-    // }
+    public static Config? ReadConfig()
+    {
+        string configFile = GetConfigFilePath();
+        if (File.Exists(configFile) == false)
+        {
+            return null;
+        }
+    
+        using (var reader = new StreamReader(configFile, Encoding.UTF8))
+        {
+            // Read settings
+            string buf = reader.ReadToEnd();
+            
+            // Deserialize and return
+            // var js = new System.Web.Script.Serialization.JavaScriptSerializer(); // Not found System.Web.Extensions (Visual Studio as well)
+            // var js = JsonConvert.DeserializeObject(json);
+            return JsonConvert.DeserializeObject<Config>(buf); // Use "Newtonsoft.Json" instead of System.Web.Extensions
+        }
+    }
 
-    // public static void WriteConfig(Config cfg)
-    // {
-    //     string buf = JsonConvert.SerializeObject(cfg);
-    //     string configFile = GetConfigFilePath();
-    //
-    //     using (var writer = new StreamWriter(configFile, false, Encoding.UTF8))
-    //     {
-    //         writer.Write(buf);
-    //     }
-    // }
+    public static void WriteConfig(Config cfg)
+    {
+        string buf = JsonConvert.SerializeObject(cfg);
+        string configFile = GetConfigFilePath();
+    
+        using (var writer = new StreamWriter(configFile, false, Encoding.UTF8))
+        {
+            writer.Write(buf);
+        }
+    }
 }
