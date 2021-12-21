19d4b0d42f9ae4812b25b1e88f5297170572c57f
hideru <jota.16.stigma.world@gmail.com>
2021-12-21 18:58:00 +0900

    introduced system.text.json, but app finished forcibly

--- WpfApp11/Config.cs
+++ WpfApp11/Config.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using Newtonsoft.Json;
+using System.Text.Json;
+// using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace WpfApp11;
 
@@ -25,19 +27,9 @@ public class Config
     // public static void GetConfigFilePath()
     {
         // .exe location
-        // string appFilePath = System.Reflection.Assembly.GetEntryAssembly().Location; // Dereference of a possibly null reference
-        // return System.Text.RegularExpressions.Regex.Replace(appFilePath, ".exe", "json",
-        //     System.Text.RegularExpressions.RegexOptions.IgnoreCase);
-        try
-        {
-            string appFilePath = System.Reflection.Assembly.GetEntryAssembly().Location; // Dereference of a possibly null reference
-            return System.Text.RegularExpressions.Regex.Replace(appFilePath, ".exe", "json",
-                System.Text.RegularExpressions.RegexOptions.IgnoreCase);
-        }
-        catch (Exception e)
-        {
-            return e.Message;
-        }
+        string appFilePath = System.Reflection.Assembly.GetEntryAssembly().Location; // Dereference of a possibly null reference
+        return System.Text.RegularExpressions.Regex.Replace(appFilePath, ".exe", "json",
+            System.Text.RegularExpressions.RegexOptions.IgnoreCase);
     }
 
     // public static Config ReadConfig() // Possible null reference return
@@ -57,13 +49,17 @@ public class Config
             // Deserialize and return
             // var js = new System.Web.Script.Serialization.JavaScriptSerializer(); // Not found System.Web.Extensions (Visual Studio as well)
             // var js = JsonConvert.DeserializeObject(json);
-            return JsonConvert.DeserializeObject<Config>(buf); // Use "Newtonsoft.Json" instead of System.Web.Extensions
+            // return JsonConvert.DeserializeObject<Config>(buf); // Use "Newtonsoft.Json" instead of System.Web.Extensions
+            // WeatherForecast weatherForecast = 
+            //     JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+            return JsonSerializer.Deserialize<Config>(buf);
         }
     }
 
     public static void WriteConfig(Config cfg)
     {
-        string buf = JsonConvert.SerializeObject(cfg);
+        // string buf = JsonConvert.SerializeObject(cfg);
+        string buf = JsonSerializer.Serialize(cfg);
         string configFile = GetConfigFilePath();
     
         using (var writer = new StreamWriter(configFile, false, Encoding.UTF8))
